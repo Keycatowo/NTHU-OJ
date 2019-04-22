a1e58c7419611af1fbd291099379f6a850ba780a
Keycat <owo.nthu@outlook.com>
2019-04-22 21:11:52 +0800

    時間優化，以prefix sum array儲存次數 針對長度1字元另做考慮

--- 1650-I2P(I) 2019_Spring_Chen_Midterm1_Practice/12140/12140 - HaSaKi.c
+++ 1650-I2P(I) 2019_Spring_Chen_Midterm1_Practice/12140/12140 - HaSaKi.c
@@ -1,25 +1,33 @@
 #include <stdio.h>
-char S[1000];
-char s[1000];
+char S[1000];//hasahasasaki
+char s[1000];//sa
+int times[1001];//to store how many times s in S 1~1000
 
-
-int getline(char*);
-void show(char*,int);
-int Find(int,int,int);
+int getline(char* Seq);
+void show(char* Seq,int length);
+int Find(int start,int end,int length);
+void build(int N,int n);
+int count_times(int l,int r,int n);
 
 
 
 int main(){
+    /// input S s
     freopen("12140_sampleIn.txt","r",stdin);
     int N,n;
     N=getline(S);
     n=getline(s);
+
+    /// Build times array
+    build(N,n);
+
+    /// t queries
     int t,l,r;
     scanf("%d",&t);
     for(int i=0;i<t;i++){
         scanf("%d %d",&l,&r);
-        printf("%d\n",Find(l-1,r-1,n));
-
+        //printf("%d\n",Find(l-1,r-1,n));
+        printf("%d\n",count_times(l,r,n));
     }
     return 0;
 }
@@ -44,6 +52,15 @@ void show(char* Seq,int length){
 int Find(int start,int end,int length){
     int sum=0;
     int check=0,j=0;
+    // lengthg(s)=1
+    if(length==1)
+    {
+        for(int i=start;i<=end;i++){
+            if(S[i]==s[0]) sum++;
+        }
+        return sum;
+    }
+
     for(int i=start;i<=end;i++){
         // judge each equal
         if(S[i]==s[j])
@@ -56,7 +73,7 @@ int Find(int start,int end,int length){
             check=0;
             j=0;
         }
-        // count each all s
+        // count each all substring(s) in sting(S)
         if(check==length){
             sum++;
             check=0;
@@ -65,4 +82,21 @@ int Find(int start,int end,int length){
     }
     //printf("%d\n",sum);
     return sum;
+}
+void build(int N,int n){
+    for(int i=1;i<=N;i++){
+        times[i]=Find(0,i-1,n);
+    }
+}
+int count_times(int l,int r,int n){
+    if(n>1)
+    {
+        return (times[r]-times[l]);
+    }
+    else
+    {
+        //if (l==r) return (times[l]-times[l-1]);
+        return (times[r]-times[l-1]);
+    }
+
 }
