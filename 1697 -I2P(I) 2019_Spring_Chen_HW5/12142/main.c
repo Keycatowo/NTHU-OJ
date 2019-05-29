77dbed4b40bc4b70a9cdfdb7720abc4e785c970f
Keycat <owo.nthu@outlook.com>
2019-05-29 18:08:25 +0800

    解決輸入問題 原本的輸入方式會導致少吃到一行 所以先gets一次消掉它

---
+++ 1697 -I2P(I) 2019_Spring_Chen_HW5/12142/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define SIZE 100
+#define MAX 1000
+
+char strs[SIZE][MAX];
+char *ptrs[SIZE];
+
+int compare_str_ptr(const void *a, const void *b)
+{
+    char **pa;
+    char **pb;
+    pa = (char **) a;
+    pb = (char **) b;
+    return strcmp(*pa, *pb);
+}
+
+
+int main(void)
+{
+    int i;
+    int n;
+    freopen("sample.txt","r",stdin);
+    ///input
+    scanf("%d",&n);
+    gets(strs[0]);
+    for (i=0; i<n; i++){
+        gets(strs[i]);
+    }
+
+
+
+
+    printf("strs: ");
+    for (i=0; i<=n; i++) {
+        printf("%s ", strs[i]);
+    }
+    printf("\n");
+
+    for (i=0; i<SIZE; i++) {
+        ptrs[i] = strs[i];
+    }
+
+    printf("ptrs: ");
+    for (i=0; i<n; i++) {
+        printf("%p ", ptrs[i]);
+    }
+    printf("\nafter sorting\n");
+
+    qsort(ptrs, n, sizeof(char*), compare_str_ptr);
+
+    printf("ptrs: ");
+    for (i=0; i<n; i++) {
+        printf("%p ", ptrs[i]);
+    }
+    printf("\n");
+
+    printf("ptrs: ");
+    for (i=0; i<n; i++) {
+        printf("%s ", ptrs[i]);
+    }
+    printf("\n");
+
+    printf("strs: ");
+    for (i=0; i<n; i++) {
+        printf("%s ", strs[i]);
+    }
+
+    return 0;
+}
