9123e8856f74381415da6001be1e8416ec056bcf
Keycat <owo.nthu@outlook.com>
2019-05-30 14:56:57 +0800

    12155 DFS解法

---
+++ 1697 -I2P(I) 2019_Spring_Chen_HW5/12155/DFS.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <math.h>
+#define MAX 1005
+
+int n,r,sum=0,one=0;
+int x[MAX],y[MAX];
+bool adj[MAX][MAX];
+bool visited[MAX];
+int group[MAX];
+
+
+bool DFS(int i){
+    bool is_leaf = true;
+    for(int j=0;j<n;j++)
+        if(adj[i][j] && !visited[j])
+        {
+            visited[j] = true;
+            is_leaf = false;
+            DFS(j);
+        }
+//    if(is_leaf)
+//        sum++;
+    return is_leaf;
+}
+
+void traversal(){
+    ///reset
+    memset(visited,false,n);
+    memset(group,MAX,n);
+    for(int i=0;i<n;i++)
+        if(!visited[i])
+        {
+            visited[i] = true;
+            if(DFS(i))
+                one++;
+            else
+                sum++;
+        }
+}
+
+void input(){
+    scanf("%d %d",&n,&r);
+    for(int i=0;i<n;i++)
+        scanf("%d %d",&x[i],&y[i]);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if( (x[i]-x[j])*(x[i]-x[j])
+               +(y[i]-y[j])*(y[i]-y[j])
+               <= r*r )
+                adj[i][j]=adj[j][i]=true;
+        }
+    }
+//
+//    for(int i=0;i<n;i++)
+//    {
+//        bool nothing = true;
+//        for(int j=0;j<n;j++)
+//            if(adj[i][j])
+//                nothing = false;
+//        if(nothing) one++;
+//    }
+}
+
+
+int main(){
+    freopen("sample.txt","r",stdin);
+    input();
+    traversal();
+    printf("%d %d\n",one,sum);
+    return 0;
+}
